00474a7e208a8ea9a38b071f03505f7d89142d1b
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-19 17:48:26 +0545

    feat(users): add authentication and password reset routes

---
+++ src/apps/users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    RegisterView,
+    VerifyEmailView,
+    LoginView,
+    ForgotPasswordView,
+    ResetPasswordView,
+)
+
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="user-register"),
+    path("verify-email/", VerifyEmailView.as_view(), name="verify-email"),
+    path("login/", LoginView.as_view(), name="user-login"),
+    path("forgot-password/", ForgotPasswordView.as_view(), name="forgot-password"),
+    path("reset-password/", ResetPasswordView.as_view(), name="reset-password"),
+]
