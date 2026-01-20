8e6aff89fdc057b36bf82d09b3b6d488c7eb264b
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-20 18:47:28 +0545

    feat(company): create a basic company model

--- src/apps/companies/models.py
+++ src/apps/companies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Company(models.Model):
+    name=models.CharField(max_length=250, unique=True)
+    description=models.TextField(blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    owner=models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_companies")
+
+    def __str__(self):
+        return self.name
+
+
