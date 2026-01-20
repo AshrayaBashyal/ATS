9c802827902174f39b7c70391941372776f3efc7
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-20 19:02:03 +0545

    feat(company): create a basic company-member model

--- src/apps/companies/models.py
+++ src/apps/companies/models.py
@@ -13,4 +13,21 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
+class CompanyMember(models.Model):
+    ADMIN="admin"
+    MANAGER="manager"
+    RECRUITER="recruiter"
+    ROLE_CHOICES=[
+        (ADMIN,"Admin"),
+        (MANAGER,"Manager"),
+        (RECRUITER,"Recruiter")
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="members")
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default=RECRUITER)
+    joined_at=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "company")
 
