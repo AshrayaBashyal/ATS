59a5860afb221484ea21a8ec8558621dbc6671a4
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-23 20:21:45 +0545

    fix import error and make migrations and fix settings for schema

--- src/apps/companies/tasks.py
+++ src/apps/companies/tasks.py
@@ -1,16 +1,16 @@
 from celery import shared_task
 from apps.emails.services import send_email
-from .models import CompanyInvite
+from .models import Invite
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={"max_retries": 3, "countdown": 10})
 def send_invite_email_task(self, invite_id: int):
     try:
         # select_related avoids extra DB queries for company details
-        invite = CompanyInvite.objects.select_related('company').get(id=invite_id)
+        invite = Invite.objects.select_related('company').get(id=invite_id)
         
         # invite = CompanyInvite.objects.get(id=invite_id)
 
-    except CompanyInvite.DoesNotExist:
+    except Invite.DoesNotExist:
         return f"Invite {invite_id} not found."
 
     subject = f"You are invited to join {invite.company.name}"
