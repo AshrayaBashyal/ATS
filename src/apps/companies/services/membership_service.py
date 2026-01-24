4ab4a884cdd702f6d23161e13d91ea6159bf0712
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-24 13:02:09 +0545

    fix race condition in remove_member and change_member_role services using db locks

--- src/apps/companies/services/membership_service.py
+++ src/apps/companies/services/membership_service.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from apps.companies.models import Membership
+from django.db import transaction
 
 
 def change_member_role(*, membership, new_role, changed_by):
@@ -8,19 +9,21 @@ def change_member_role(*, membership, new_role, changed_by):
     Only admins can change roles.
     Cannot remove last admin.
     """
+    with transaction.atomic():
+        membership = Membership.objects.select_for_update().get(id=membership.id)
 
-    if membership.role == Membership.Role.ADMIN:
-        total_admins = Membership.objects.filter(
-            company=membership.company,
-            role=Membership.Role.ADMIN
-        ).count()
+        if membership.role == Membership.Role.ADMIN:
+            total_admins = Membership.objects.filter(
+                company=membership.company,
+                role=Membership.Role.ADMIN
+            ).count()
 
-        if total_admins == 1:
-            raise ValidationError("Cannot change role of the last admin.")
+            if total_admins == 1:
+                raise ValidationError("Cannot change role of the last admin.")
 
-    membership.role = new_role
-    membership.save(update_fields=["role"])
-    return membership
+        membership.role = new_role
+        membership.save(update_fields=["role"])
+        return membership
 
 
 def remove_member(*, membership, removed_by):
@@ -28,14 +31,16 @@ def remove_member(*, membership, removed_by):
     Remove a member from company.
     Prevent removing last admin.
     """
+    with transaction.atomic():
+        membership = Membership.objects.select_for_update().get(id=membership.id)
 
-    if membership.role == Membership.Role.ADMIN:
-        total_admins = Membership.objects.filter(
-            company=membership.company,
-            role=Membership.Role.ADMIN
-        ).count()
+        if membership.role == Membership.Role.ADMIN:
+            total_admins = Membership.objects.filter(
+                company=membership.company,
+                role=Membership.Role.ADMIN
+            ).count()
 
-        if total_admins == 1:
-            raise ValidationError("Cannot remove the last admin.")
+            if total_admins == 1:
+                raise ValidationError("Cannot remove the last admin.")
 
-    membership.delete()
+        membership.delete()
