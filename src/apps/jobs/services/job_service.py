e4049cd7a5d35f798d9ec41071a5cc6bd7cd18a8
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-05 17:49:26 +0545

    create service for role based job updation

--- src/apps/jobs/services/job_service.py
+++ src/apps/jobs/services/job_service.py
@@ -27,4 +27,21 @@ def create_job(*, company, title, description, department, location, created_by)
     return job
 
 
+def update_job(*, job, data, updated_by):
+    """
+    Only Admin or Recruiter can update job.
+    """
+
+    if not Membership.objects.filter(
+        company=job.company,
+        user=updated_by,
+        role__in=[Membership.Role.ADMIN, Membership.Role.RECRUITER]
+    ).exists():
+        raise ValidationError("You cannot update this job.")
+
+    for field, value in data.items():
+        setattr(job, field, value)
+
+    job.save()
+    return job
 
