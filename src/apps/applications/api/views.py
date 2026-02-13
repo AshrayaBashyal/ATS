9635a40d3f4edfd478a9c833fefc33e05960651a
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-13 23:18:34 +0545

    register application list funcn

--- src/apps/applications/api/views.py
+++ src/apps/applications/api/views.py
@@ -66,3 +66,9 @@ class ApplicationViewset(viewsets.GenericViewSet):
             self.get_serializer(application).data,
             status=status.HTTP_200_OK,
         )
+
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
